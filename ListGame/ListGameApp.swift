93897245aef88754075b68dacfce0892b5370787
Nathan Hughes <nathanhughes@MacBookPro.attlocal.net>
2024-09-09 19:55:28 -0400

    only commit

---
+++ ListGame/ListGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListGameApp.swift
+//  ListGame
+//
+//  Created by Nathan Hughes on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ListGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ListGameTests/ListGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  ListGameTests.swift
+//  ListGameTests
+//
+//  Created by Nathan Hughes on 9/9/24.
+//
+
+import Testing
+@testable import ListGame
+
+struct ListGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
